05cf9b09737be69b58fc5ad85d3501a9604066ae
GuihCFerreira <guihhh231@gmail.com>
2025-04-04 22:18:16 -0300

    feat: add JWT token generation utility

---
+++ src/utils/token.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = async (userId: string) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET not found in .env file");
+  }
+
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+
+export { generateToken };
