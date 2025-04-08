97ef9c77f6805eccc1f11ea529f051d5bdb10bcf
GuihCFerreira <guihhh231@gmail.com>
2025-04-07 21:50:44 -0300

    feat: add token validation to user routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { validateToken } from "../middleware/validate-token";
 import userController from "../controller/user-controller";
 import { verifyIdParam } from "../middleware/verify-id-param";
 
 const userRouter = Router();
 
-userRouter.get("/", validateToken, userController.getAllUsers);
-userRouter.get("/:id", validateToken, verifyIdParam, userController.getUserById);
+userRouter.get("/", userController.getAllUsers);
+userRouter.get("/:id", verifyIdParam, userController.getUserById);
 
 export { userRouter };
