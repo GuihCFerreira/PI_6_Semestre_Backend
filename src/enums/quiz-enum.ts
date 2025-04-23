c4586596ce136de8158ae14b5d50db21e4a4775d
GuihCFerreira <guihhh231@gmail.com>
2025-04-22 21:45:45 -0300

    feat: add quiz schema and enum

---
+++ src/enums/quiz-enum.ts
@@ -0,0 +1,11 @@
+export enum QuizTableFields {
+    FAVORITE_GAMES = 'favorite_games',
+    GENRES = 'genres',
+    CATEGORIES = 'categories',
+    OPERATIONAL_SYSTEMS = 'operational_systems',
+    GAME_LANGUAGE = 'game_language',
+    PUBLISHERS = 'publishers',
+    MODE = 'game_modes',
+    STYLE = 'game_styles',
+    CAMERA = 'camera_perspective',
+}
