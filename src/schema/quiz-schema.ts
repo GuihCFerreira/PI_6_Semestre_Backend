33f240fbe6cacfca88930da5aed373dafed00e94
GuihCFerreira <guihhh231@gmail.com>
2025-04-22 21:45:45 -0300

    feat: add quiz schema and enum

---
+++ src/schema/quiz-schema.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { QuizTableFields } from "../enums/quiz-enum";
+
+const baseQuizSchema = z.object({
+    FAVORITE_GAMES: z.array(z.string()).default([]),
+    GENRES: z.array(z.string()).optional().default([]),
+    CATEGORIES: z.array(z.string()).optional().default([]),
+    OPERATIONAL_SYSTEMS: z.array(z.string()).default([]),
+    GAME_LANGUAGE: z.string().default(""),
+    PUBLISHERS: z.array(z.string()).optional().default([]),
+    MODE: z.array(z.string()).optional().default([]),
+    STYLE: z.array(z.string()).optional().default([]),
+    CAMERA: z.array(z.string()).optional().default([]),
+})
+
+const createQuizSchema = baseQuizSchema.transform((data) => {
+    const transformed: Record<string, any> = {};
+
+    for (const key in QuizTableFields) {
+        const dbField = QuizTableFields[key as keyof typeof QuizTableFields];
+        transformed[dbField] = data[key as keyof typeof data];
+    }
+
+    return transformed;
+})
+
+const updateQuizSchema = baseQuizSchema.partial().transform((data) => {
+    const transformed: Record<string, any> = {};
+
+    for (const key in QuizTableFields) {
+        const dbField = QuizTableFields[key as keyof typeof QuizTableFields];
+        transformed[dbField] = data[key as keyof typeof data];
+    }
+
+    return transformed;
+});
+
+export { createQuizSchema, updateQuizSchema };
