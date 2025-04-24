48a4e9de25c0e9c8fa821081c03e1b1ffdb4c79e
GuihCFerreira <guihhh231@gmail.com>
2025-04-23 23:59:29 -0300

    feat: add game suggested schema

---
+++ src/schema/game-suggested-schema.ts
@@ -0,0 +1,17 @@
+import { release } from "os";
+import { z } from "zod";
+
+const createGameSuggestedSchema = z.object({
+    quiz_id: z.string().uuid(),
+    game_id: z.string().uuid(),
+    name: z.string().min(1).max(100),
+    short_description: z.string().min(1).max(500),
+    header_image: z.string().url(),
+    release_date: z.string().optional(),
+})
+
+const updateGameSuggestedSchema = createGameSuggestedSchema.partial()
+
+const createManyGameSuggestedSchema = z.array(createGameSuggestedSchema)
+
+export { createGameSuggestedSchema, updateGameSuggestedSchema, createManyGameSuggestedSchema }
