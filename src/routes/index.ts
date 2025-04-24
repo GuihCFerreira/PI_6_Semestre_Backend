cdb8cd1b94074192f65af1cb0f2c41b2ceb92a0c
GuihCFerreira <guihhh231@gmail.com>
2025-04-23 23:59:46 -0300

    feat: add game routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,14 @@ import { userRouter } from "./user-routes";
 import { validateToken } from "../middleware/validate-token";
 import { quizRouter } from "./quiz-routes";
 import { notFoundRoute } from "../middleware/not-found-route";
+import { gamesRouter } from "./games-routes";
 
 const router: Router = Router();
 
 router.use("/", authRouter);
 router.use("/user", validateToken, userRouter);
 router.use("/quiz", validateToken, quizRouter);
+router.use("/games", validateToken, gamesRouter);
 
 router.use(notFoundRoute);
 
