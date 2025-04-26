3ae8aab0edef9eb906c728e4bfe56bb6d964e626
GuihCFerreira <guihhh231@gmail.com>
2025-04-26 00:17:38 -0300

    feat: dd favorite games routes

---
+++ src/routes/favorite-games-routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import favoriteGamesController from "../controller/favorite-games-controller";
+import { verifyIdParam } from "../middleware/verify-id-param";
+import { validadeInput } from "../middleware/validate-input";
+import { addFavoriteGameSchema } from "../schema/favorite-games-schema";
+
+const favoriteGamesRouter = Router();
+
+favoriteGamesRouter.get("/", favoriteGamesController.getAllFavoriteGames);
+favoriteGamesRouter.get("/:id", verifyIdParam, favoriteGamesController.getFavoriteGameById);
+favoriteGamesRouter.post("/", validadeInput(addFavoriteGameSchema), favoriteGamesController.addFavoriteGame);
+favoriteGamesRouter.delete("/:id", verifyIdParam, favoriteGamesController.removeFavoriteGame);
+
+export { favoriteGamesRouter };
