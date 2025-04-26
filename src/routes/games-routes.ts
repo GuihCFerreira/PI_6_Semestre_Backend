6061b3d1c66fee96480691020f49a3def625680a
GuihCFerreira <guihhh231@gmail.com>
2025-04-26 00:17:38 -0300

    feat: dd favorite games routes

--- src/routes/games-routes.ts
+++ src/routes/games-routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { favoriteGamesRouter } from "./favorite-games-routes";
 
 const gamesRouter = Router();
+
+gamesRouter.use("/favorite", favoriteGamesRouter);
 
 export { gamesRouter };
