c63d862ed6f8c6c6f798bfca866af36de5a8321a
GuihCFerreira <guihhh231@gmail.com>
2025-04-28 20:40:27 -0300

    refactor: change game id type in schema

--- src/schema/game-suggested-schema.ts
+++ src/schema/game-suggested-schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 const createGameSuggestedSchema = z.object({
     quiz_id: z.string().uuid(),
-    game_id: z.string().uuid(),
+    game_id: z.number().int().positive(),
     name: z.string().min(1).max(100),
     short_description: z.string().min(1).max(500),
     header_image: z.string().url(),
