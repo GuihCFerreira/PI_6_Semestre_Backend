0de34990711adef438f0ef5db93f441d9c3144d6
GuihCFerreira <guihhh231@gmail.com>
2025-04-29 23:42:44 -0300

    refactor: change release date field type in games tables

---
+++ prisma/migrations/20250430023844_change_release_date_field_type_in_games_table/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "FavoriteGames" ALTER COLUMN "release_date" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "GameSuggested" ALTER COLUMN "release_date" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "PlayLaterGames" ALTER COLUMN "release_date" SET DATA TYPE TEXT;
