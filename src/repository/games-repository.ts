d15c8d2cb8ae425deb959ce94761c65a3264edbd
GuihCFerreira <guihhh231@gmail.com>
2025-05-06 23:37:44 -0300

    feat: add initial game routes

---
+++ src/repository/games-repository.ts
@@ -0,0 +1,14 @@
+const getGameById = async (id: number) => {
+    //TODO: Implement the logic to get a game by its ID from the python API
+    return {}
+}
+
+const getGameRecomendations = async (searchData: any) => {
+    //TODO: Implement the logic to get game recommendations from the python API
+    return []
+}
+
+export default {
+    getGameById,
+    getGameRecomendations,
+}
