17b9886af7cbd5d45833922feb9501a109f2e1c4
GuihCFerreira <guihhh231@gmail.com>
2025-05-09 22:05:57 -0300

    feat: add user image routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { validateToken } from "../middleware/validate-token";
 import userController from "../controller/user-controller";
 import { verifyIdParam } from "../middleware/verify-id-param";
+import { validateImageUpload } from "../middleware/validate-image-upload";
 
 const userRouter = Router();
 
 userRouter.get("/", validateToken, userController.getAllUsers);
 userRouter.get("/:id", validateToken, verifyIdParam, userController.getUserById);
+userRouter.post("/image", validateToken, validateImageUpload, userController.uploadUserImage);
+userRouter.delete("/image", validateToken, userController.deleteUserImage);
 
 export { userRouter };
--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,3 +1,4 @@
+import { NotFound } from "../error/not-found-error"
 import userRepository from "../repository/user-repository"
 
 const getAllUsers = async () => {
@@ -8,4 +9,41 @@ const getUserById = async (id: string) => {
     return userRepository.getUserById(id)
 }
 
-export default { getAllUsers, getUserById }
+const uploadUserImage = async (userId: string, image: string) => {
+
+    const user = await userRepository.getUserById(userId)
+    if (!user) throw new NotFound("User not found")
+
+    const cloudinaryImage = await userRepository.uploadImageToCloudinary(image, userId)
+    if (!cloudinaryImage) throw new Error("Error uploading image to cloudinary")
+
+    const updatedUser = await userRepository.updateUserImage(userId, cloudinaryImage.secure_url, cloudinaryImage.public_id)
+    if (!updatedUser) throw new Error("Error updating user image")
+
+    return updatedUser    
+
+}
+
+const deleteUserImage = async (userId: string) => {
+
+    const user = await userRepository.getUserById(userId)
+    if (!user) throw new NotFound("User not found")
+
+    if (!user.photo_id) throw new Error("User does not have an image")
+
+    const deletedImage = await userRepository.deleteImageFromCloudinary(user.photo_id)
+    if (!deletedImage) throw new Error("Error deleting image from cloudinary")
+
+    const updatedUser = await userRepository.updateUserImage(userId, null, null)
+    if (!updatedUser) throw new Error("Error updating user image")
+        
+    return updatedUser
+
+}
+
+export default { 
+    getAllUsers, 
+    getUserById,
+    uploadUserImage,
+    deleteUserImage 
+}
