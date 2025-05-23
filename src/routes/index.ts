011aba91a2691c4b2ba1b6f4f1368dad32b367b4
GuihCFerreira <guihhh231@gmail.com>
2025-05-23 00:57:56 -0300

    feat: add initial API documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,15 @@ import { validateToken } from "../middleware/validate-token";
 import { quizRouter } from "./quiz-routes";
 import { notFoundRoute } from "../middleware/not-found-route";
 import { gamesRouter } from "./games-routes";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 const router: Router = Router();
+
+const swaggerDocument = YAML.load(path.join(__dirname, '..', 'docs', 'api.yaml'));
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 router.use("/", authRouter);
 router.use("/user", validateToken, userRouter);
